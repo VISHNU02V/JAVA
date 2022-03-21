4df181547be8925f3b33d9d3fd3111bb07a3699e
Vishnu V <84262256+VISHNU02V@users.noreply.github.com>
2022-03-21 22:21:10 +0530

    Update Filehandling1.java

--- Filehandling1.java
+++ Filehandling1.java
@@ -1,11 +1,11 @@
 //Write a program to added prime numbers to a file named prime.data and added the composite number to the composite.data
 
 import java.io.*;
-import java.util.Scanner;
+import java.util.Scanner;                                                                                   //    importing .................
 
 class Filehandling1 {
     public static void main(String[] args) throws Exception {
-        int c[] = new int[30];
+        int c[] = new int[30];                                                                              //    intialising ...............
         int p[] = new int[30];
         int co[] = new int[30];
         int in[] = new int[30];
@@ -17,18 +17,18 @@ class Filehandling1 {
         System.out.println();
         System.out.println("INPUTING ......................");
         System.out.println("");
-        System.out.println("enter the number of iterations");
+        System.out.println("enter the number of iterations");                                               //   inputing the limit ......
         Scanner a1 = new Scanner(System.in);
         int a = a1.nextInt();
         System.out.println();
-        System.out.println("enter the elements in to the array:");
+        System.out.println("enter the elements in to the array:");                                          //   inputing the element ....
         for (int i = 0; i < a; i++) {
             Scanner x = new Scanner(System.in);
             int v = x.nextInt();
             c[i] = v;
         }
         System.out.println();
-        System.out.println("DISPLAYING THE ARRAY ");
+        System.out.println("DISPLAYING THE ARRAY ");                                                        //   displaying the array .....
         System.out.println("************************");
         System.out.println();
         System.out.print(" [ ");
@@ -46,8 +46,8 @@ class Filehandling1 {
         for (int i = 0; i < a; i++) {
             int s = 0;
             if (c[i] == 1) {
-                System.out.println("1 is neither compositive nor prime number");
-            } else {
+                System.out.println("1 is neither compositive nor prime number");                            //  displaying 1 as it is neither compositive
+            } else {                                                                                        //  nor prime number ......................
                 for (int j = 1; j < c[i]; j++) {
                     if (c[i] % j == 0) {
                         s = s + 1;
@@ -66,14 +66,14 @@ class Filehandling1 {
                 }
             }
         }
-        System.out.print("PRIME NUMBER :: ");
+        System.out.print("PRIME NUMBER :: ");                                                               //   displaying the prime number ...........
         System.out.print("[ ");
         for (int i = 0; i < z; i++) {
             System.out.print(" " + p[i]);
         }
         System.out.print(" ] ");
         System.out.println();
-        System.out.print("COMPOSITE NUMBER :: ");
+        System.out.print("COMPOSITE NUMBER :: ");                                                           //  displaying the composite number ......... 
         System.out.print("[ ");
         for (int i = 0; i < k; i++) {
             System.out.print(" " + co[i]);
@@ -83,7 +83,7 @@ class Filehandling1 {
         FileReader ch2 = new FileReader(b);
         System.out.println();
         System.out.println();
-        System.out.println("/// OUTPUT FROM FILE ///");
+        System.out.println("/// OUTPUT FROM FILE ///");                                                      //  output in the file ......................
         System.out.println("************************");
         System.out.println();
         if (f1 != null) {
@@ -97,7 +97,7 @@ class Filehandling1 {
         System.out.println("*************************** ***************************");
         System.out.println("                       THANK YOU ");
         ch1.close();
-        f1.close();
+        f1.close();                                                                                           //    closing all the files .................
         f2.close();
         ch2.close();
 
